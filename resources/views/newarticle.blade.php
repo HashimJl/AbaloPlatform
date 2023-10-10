78c3fcd36f961841985e12c415a56bcb059d2abb
HashimJl <95547638+HashimJl@users.noreply.github.com>
2023-10-10 16:35:52 +0200

    insert new article via AJAX

--- resources/views/newarticle.blade.php
+++ resources/views/newarticle.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'insert article')
 
 @section('content')
-    <form class="row g-3" method="post" action="/articles">
+    <form class="row g-3" id="newArticle" method="post" action="/articles">
         <div class="col-md-6">
             <label for="name" class="form-label">Article Name:</label>
             <input type="text" class="form-control" id="name" name="name" required>
@@ -21,4 +21,31 @@
             <button type="submit" class="btn btn-primary">Insert to marketplace</button>
         </div>
     </form>
+
+    <script>
+        window.addEventListener('load', () => {
+            function sendData() {
+                let xhr = new XMLHttpRequest();
+                const form = document.getElementById('newArticle');
+                const data = new FormData(form);
+
+                xhr.open('POST', '/articles');
+
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState === 4) {
+                        if (xhr.status === 200) {
+                            console.log(xhr.responseText);
+                        } else {
+                            console.error(xhr.statusText);
+                        }
+                    }
+                }
+                xhr.send(data);
+            }
+            form.addEventListener('submit', (event) => {
+                event.preventDefault();
+                sendData();
+            });
+        })
+    </script>
 @endsection
